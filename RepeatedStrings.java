c1d24b769e0a15c6039d8e9c1e9bc1553ac693d9
davidkatz <15Dkatz@shcp.edu>
2016-09-30 21:09:38 -0700

    add repeatedStrings

---
+++ RepeatedStrings.java
@@ -0,0 +1,55 @@
+package ctciHackerrank;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+//Sample Input 1
+//
+//a
+//1000000000000
+//Sample Output 1
+//
+//1000000000000
+
+public class RepeatedStrings {
+	/**
+	 * Repeat the s string n times and count a's
+	 *
+	 * @param s
+	 *            s whose value is the input string
+	 * @param n
+	 *            whose values is the number of times s repeats
+	 * @return the number of a's in s.
+	 */
+	public static long aNum(String s, long n) {
+		// you don't need to build a new string...
+		// but you can repeat the string length up to n
+		// multiply this number * the number of a's in the s
+		// take the remainder and create a substring of s
+		// add the number of a's in this substring to the original count
+		String nonAs = s.replaceAll("[a]++", "");
+		int numAs = s.length() - nonAs.length();
+
+		long allAs = numAs * n / s.length();
+
+		long subLength = n % s.length();
+		// int intSL = new BigDecimal(subLength).intValueExact();
+		int remainder = new BigDecimal(subLength).intValueExact();
+		String subS = s.substring(0, remainder);
+		subS = subS.replaceAll("[a]++", "");
+
+		int lastAs = remainder - subS.length();
+		// int trueAs = new BigDecimal(allAs).intValueExact();
+		allAs += lastAs;
+
+		return allAs;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		long n = in.nextLong();
+
+		System.out.println(aNum(s, n));
+	}
+}
---
+++ arrayLeftRotation.java
@@ -0,0 +1,88 @@
+package ctciHackerrank;
+
+import java.util.Scanner;
+
+//5 4
+//1 2 3 4 5
+
+//5 1 2 3 4
+
+public class arrayLeftRotation {
+	/**
+	 * Rotate the array left 1 time
+	 *
+	 * @param array
+	 * @return the rotated array
+	 */
+	public static int[] leftRotate(int[] array) {
+		int[] newArr = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+			if (i == array.length - 1) {
+				newArr[i] = array[0];
+			} else {
+				newArr[i] = array[i + 1];
+			}
+		}
+		array = newArr;
+		return array;
+	}
+
+	/**
+	 * Rotate the array by a number of rotations through recursion
+	 *
+	 * @param array
+	 * @param rotations
+	 * @return the rotated array
+	 */
+	public static int[] multiLeftRotate(int[] array, int rotations) {
+		for (int i = 0; i < rotations; i++) {
+			array = leftRotate(array);
+		}
+		return array;
+	}
+
+	/**
+	 * print the array
+	 *
+	 * @param array
+	 */
+	public static void print(int[] array) {
+		for (int p = 0; p < array.length; p++) {
+			System.out.print(array[p] + " ");
+		}
+	}
+
+	/**
+	 * Quick rotation of the array
+	 *
+	 * @param array
+	 * @param rotations
+	 * @return the array
+	 */
+	public static int[] quickRotate(int[] array, int rotations) {
+		int[] newArr = new int[array.length];
+
+		for (int i = 0; i < array.length; i++) {
+			int newPosition = i - rotations;
+			if (newPosition < 0) {
+				newPosition += array.length;
+			} else {
+				newPosition = newPosition;
+			}
+			newArr[newPosition] = array[i];
+		}
+		return newArr;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int k = in.nextInt();
+		int a[] = new int[n];
+		for (int a_i = 0; a_i < n; a_i++) {
+			a[a_i] = in.nextInt();
+		}
+		a = quickRotate(a, k);
+		print(a);
+	}
+}
