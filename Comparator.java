d78141643505b4d3970f74e372a13cb9ee1abc3d
davidkatz <15Dkatz@shcp.edu>
2016-10-02 15:05:16 -0700

    add Comparator

---
+++ Comparator.java
@@ -0,0 +1,16 @@
+// import java.util.Comparator;
+//
+// class Checker implements Comparator<Player> {
+//
+// @Override
+// public int compare(Player a, Player b) {
+// if (a.score == b.score) {
+// return a.name.compareTo(b.name);
+// }
+// if (a.score < b.score) {
+// return 1;
+// } else {
+// return -1;
+// }
+// }
+// }
